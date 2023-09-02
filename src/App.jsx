9e20998634e80f03bba82eb4af9a85550a3846e4
Masum Rayhan <masumrayhan42@gmail.com>
2023-09-03 00:20:03 +0600

    getUsers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { db } from "./utils/firebase/firebase-util";
+import "./App.css";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, "users");
 
-  return (
-    <div className='App'>
+  useEffect(() => {
+    const getUSers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    };
 
-    </div>
-  )
+    getUSers();
+  }, []);
+  return <div className="App">
+    {users.map((user) => {
+      return <div key={user.id}>
+        <h1>Name: {user.name}</h1>
+        <h1>Age: {user.age}</h1>
+      </div>
+    })}
+  </div>;
 }
 
-export default App
+export default App;
